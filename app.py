1ccfc76efef30123e4cc363a2facc6c86cedfb21
nilson-blip <nilsondeabreu.lp@gmail.com>
2026-09-17 14:52:54 -0300

    Fix indentation and update payment processing logic

--- app.py
+++ app.py
@@ -130,8 +130,7 @@ async def botoes(update: Update, context):
 
             print("MERCADO PAGO:", response.status_code)
             print(response.text)
-
-                        response.raise_for_status()
+            response.raise_for_status()
             order = response.json()
             payment_url = order["transactions"]["payments"][0]["payment_method"]["ticket_url"]
             order_id = order["id"]
@@ -141,8 +140,8 @@ async def botoes(update: Update, context):
                 "telegram_user_id": query.from_user.id,
                 "amount": 1.00,
                 "status": "pending",
-                "external_reference": order["external_reference"] , 
-                "dias_acesso: 30
+                "external_reference": order["external_reference"],
+                 "dias_acesso": 30
             }).execute()
 
             print(f"💾 PAGAMENTO REGISTRADO NO SUPABASE: {order_id}")
