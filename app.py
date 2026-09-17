21d42031f9c31eeeeefc9f2ed6152a7f5125a6b2
nilson-blip <nilsondeabreu.lp@gmail.com>
2026-09-17 16:14:12 -0300

    Add import statement for PlainTextResponse

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import mercadopago
 
 from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
+
 app = FastAPI()
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
