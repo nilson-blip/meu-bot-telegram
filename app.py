d535f30cf3d2d633d974b01c3d272ddb602c8a34
nilson-blip <nilsondeabreu.lp@gmail.com>
2026-09-17 16:46:10 -0300

    Add print statement for app loading confirmation

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
+print("BOTCHE APP CARREGADO")
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
