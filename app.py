1195b1406c6ada74959ac41f87c14e05b7ac7223
nilson-blip <nilsondeabreu.lp@gmail.com>
2026-09-20 15:28:43 -0300

    Reformat code for payment registration in app.py

--- app.py
+++ app.py
@@ -331,12 +331,11 @@ async def botoes(update: Update, context):
     "dias_acesso": 30,
     "data_expiracao": None,
     "payment_url": payment_url,
-    "remarketing_enviado": False
-
-"client_id": 1,
-"product_id": 1,
-"vip_group_id": 1,
-"payment_connection_id": 1,
+    "remarketing_enviado": False,
+    "client_id": 1,
+    "product_id": 1,
+    "vip_group_id": 1,
+    "payment_connection_id": 1
 }).execute()
 
             print(
