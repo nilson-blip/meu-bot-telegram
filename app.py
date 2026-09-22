aefcdceeabfbec6e19ff78681b8c50590159eab0
nilson-blip <nilsondeabreu.lp@gmail.com>
2026-09-22 03:17:37 -0300

    Refactor access registration call in app.py

--- app.py
+++ app.py
@@ -719,10 +719,8 @@ async def mercadopago_webhook(request: Request):
             ).execute()             
             
             await registrar_acesso(
-            telegram_user_id,
             
-           
-                pagamento_atual["id"]
+            telegram_user_id,pagamento_atual["id"]
             )
                                 # CRIA ASSINATURA
                 
