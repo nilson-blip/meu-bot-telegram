ed7ff0412aeddd42b8cc87a0d370bf7d930084bb
nilson-blip <nilsondeabreu.lp@gmail.com>
2026-09-24 07:43:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -233,7 +233,7 @@ else:
 
         print(f"🆕 ACESSO CRIADO: {telegram_user_id}")
       
-    supabase.table("access_control").update({
+        supabase.table("access_control").update({
         "aviso_10_enviado": False,
         "aviso_5_enviado": False,
         "aviso_3_enviado": False,
